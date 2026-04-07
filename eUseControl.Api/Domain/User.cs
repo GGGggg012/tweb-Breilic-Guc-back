becdf680eb780da2cd0a7419c43ac8530220e19f
MaxiMGuc <142231064+MaxiMGuc@users.noreply.github.com>
2026-04-08 00:01:21 +0300

    First commit - creation of user controller CRUD

---
+++ eUseControl.Api/Domain/User.cs
@@ -0,0 +1,9 @@
+namespace eUseControl.Api.Domain {
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+    }
+}
