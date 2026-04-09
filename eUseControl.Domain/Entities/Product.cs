88ea26c40a6b3cf66776ccce929153af1b8378ad
GGGggg012 <ggpuregg12@gmail.com>
2026-04-09 15:45:00 +0300

    add methods to Product

--- eUseControl.Domain/Entities/Product.cs
+++ eUseControl.Domain/Entities/Product.cs
@@ -7,5 +7,17 @@ namespace eUseControl.Domain.Entities
         public string Description { get; set; } = string.Empty;
         public decimal Price { get; set; }
         public int Stock { get; set; }
+
+        public bool IsAvailable()
+        {
+            return Stock > 0;
+        }
+
+        public void DecreaseStock(int amount)
+        {
+            if (amount > Stock)
+                throw new Exception("Not enough stock");
+            Stock -= amount;
+        }
     }
 }
