f4f587883e6f7add3f0bf870796f1aa704bace7e
GGGggg012 <ggpuregg12@gmail.com>
2026-04-11 16:19:00 +0300

    add AppDbContext

---
+++ eUseControl.DataAccess/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+using eUseControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace eUseControl.DataAccess.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<UserData> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
