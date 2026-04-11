ae3216461f45f519b5f7f4edda60ed51b3b99695
GGGggg012 <ggpuregg12@gmail.com>
2026-04-11 11:18:00 +0300

    fix UserView missing fields

--- eUseControl.Model/UserView.cs
+++ eUseControl.Model/UserView.cs
@@ -8,6 +8,8 @@ namespace eUseControl.Model
         public string FirstName { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
         public DateTime RegisteredOn { get; set; }
     }
 }
