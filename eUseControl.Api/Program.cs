5b02957fcbe47c9ace570fc93787d2402897400f
GGGggg012 <ggpuregg12@gmail.com>
2026-04-13 13:33:06 +0300

    setup Program.cs with DI and JWT

--- eUseControl.Api/Program.cs
+++ eUseControl.Api/Program.cs
@@ -1,9 +1,56 @@
+using eUseControl.Business;
+using eUseControl.DataAccess.Context;
+using eUseControl.DataAccess.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// database
+var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlite(connStr));
+
+// repositories
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<ProductRepository>();
+
+// business layer
+builder.Services.AddScoped<UserBusiness>();
+builder.Services.AddScoped<AuthBusiness>();
+builder.Services.AddScoped<ProductBusiness>();
+
+// jwt auth
+var jwtKey = builder.Configuration["Jwt:Key"];
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(opt =>
+    {
+        opt.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey)),
+            ValidateIssuer = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidateAudience = true,
+            ValidAudience = builder.Configuration["Jwt:Audience"]
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+// migrate on startup
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,6 +59,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
