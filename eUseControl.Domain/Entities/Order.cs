45c2a669b0de1274d67ca3a3b9bea8837ba20a20
GGGggg012 <ggpuregg12@gmail.com>
2026-04-14 14:41:00 +0300

    add Quantity TotalPrice Status to Order

--- eUseControl.Domain/Entities/Order.cs
+++ eUseControl.Domain/Entities/Order.cs
@@ -5,5 +5,8 @@ namespace eUseControl.Domain.Entities
         public int Id { get; set; }
         public int UserId { get; set; }
         public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string Status { get; set; } = "Pending";
     }
 }
