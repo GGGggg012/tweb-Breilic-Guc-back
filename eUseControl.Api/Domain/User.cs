2669e203d4bcfafa358459e29f913c5b8f32b9b4
GGGggg012 <ggpuregg12@gmail.com>
2026-04-14 09:55:32 +0300

    remove old User from Api domain

--- eUseControl.Api/Domain/User.cs
+++ eUseControl.Api/Domain/User.cs
@@ -1,9 +1,4 @@
-namespace eUseControl.Api.Domain {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Email { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; }
-    }
+namespace eUseControl.Api.Domain
+{
+    // moved to eUseControl.Domain project
 }
