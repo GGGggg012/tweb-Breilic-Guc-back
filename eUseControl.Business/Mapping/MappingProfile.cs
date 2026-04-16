4c7f61071d9318ee0cd07a88ec435646a41a7c57
GGGggg012 <ggpuregg12@gmail.com>
2026-04-16 10:38:00 +0300

    add MappingProfile class

---
+++ eUseControl.Business/Mapping/MappingProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using eUseControl.Domain.Entities;
+using eUseControl.Model;
+
+namespace eUseControl.Business.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
