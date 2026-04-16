1589eb3b5c24d7d26b03a7f8fd20e07bd78aa6f7
GGGggg012 <ggpuregg12@gmail.com>
2026-04-16 17:04:15 +0300

    add ErrorResponse model

---
+++ eUseControl.Model/ErrorResponse.cs
@@ -0,0 +1,8 @@
+namespace eUseControl.Model
+{
+    public class ErrorResponse
+    {
+        public string Message { get; set; } = string.Empty;
+        public int StatusCode { get; set; }
+    }
+}
