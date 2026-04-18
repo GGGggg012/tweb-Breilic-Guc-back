b233d5c50c125930d88171358b7896174f977071
GGGggg012 <ggpuregg12@gmail.com>
2026-04-18 10:21:59 +0300

    fix UserView add IsActive field

--- eUseControl.Model/UserView.cs
+++ eUseControl.Model/UserView.cs
@@ -10,6 +10,7 @@ namespace eUseControl.Model
         public string Email { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
         public DateTime RegisteredOn { get; set; }
     }
 }
