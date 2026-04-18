dd7112acc411907b7fb002401f45126e6556b875
GGGggg012 <ggpuregg12@gmail.com>
2026-04-18 12:02:49 +0300

    add ChangePassword and Delete to UserBusiness

--- eUseControl.Business/UserBusiness.cs
+++ eUseControl.Business/UserBusiness.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using System.Collections.Generic;
+using System.Linq;
 using eUseControl.DataAccess.Repositories;
 using eUseControl.Model;
 
@@ -18,14 +19,14 @@ namespace eUseControl.Business
 
         public List<UserView> GetAll()
         {
-            var users = _repo.GetAll();
+            var users = _repo.GetAll().Where(u => u.IsActive).ToList();
             return _mapper.Map<List<UserView>>(users);
         }
 
         public UserView GetById(int id)
         {
             var user = _repo.GetById(id);
-            if (user == null) return null;
+            if (user == null || !user.IsActive) return null;
             return _mapper.Map<UserView>(user);
         }
 
@@ -39,6 +40,24 @@ namespace eUseControl.Business
             user.Phone = req.Phone;
             _repo.Update(user);
             return _mapper.Map<UserView>(user);
+        }
+
+        public void ChangePassword(int id, ChangePasswordRequest req)
+        {
+            var user = _repo.GetById(id);
+            if (user == null)
+                throw new System.InvalidOperationException("User not found");
+
+            if (!user.CheckPassword(req.OldPassword))
+                throw new System.UnauthorizedAccessException("Wrong current password");
+
+            user.SetPasswordHash(req.NewPassword);
+            _repo.Update(user);
+        }
+
+        public bool Delete(int id)
+        {
+            return _repo.Delete(id);
         }
     }
 }
